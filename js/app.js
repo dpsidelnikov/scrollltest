fab7ced2b5feb76daa5cc75c1b1b82e4d8d1736d
dpsidelnikov <dpsidelnikov@gmail.com>
2019-05-30 17:06:36 +0200

    Nuevo Cambio!

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function() {
+  var controller = new ScrollMagic.Controller();
+});
+
+var blockTween = new TweenMax.to('#block', 1.5, {
+  backgroundColor: 'red'
+});
+
+var containerScene = new ScrollMagic.Scene({
+    triggerElement: '#container'
+  })
+  .setTween(blockTween)
+  .addIndicators()
+  .addTo(controller);
